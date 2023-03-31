03ebe2dac83f35352f5351b0675d1a638856dff2
Ahmed Bahaa <geekbahaa1@gmail.com>
2023-03-31 19:01:28 +0200

    Adding Twitter User registration event

--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -4,18 +4,25 @@ import { Link } from 'react-router-dom'
 import '../../App.css'
 
 export default function SignUpPage() {
-    const handleSignupSubmit = () => {
-        
+    const handleSignupSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const formValues = Object.fromEntries(formData);
+        // eslint-disable-next-line no-undef
+        twq('event', 'tw-oefyk-oefyx', {
+            email_address: formValues.email,
+            conversion_id: formValues.username
+        });
     }
 
     return (
         <div className="text-center m-5-auto">
             <h2>Join us</h2>
             <h5>Create your personal account</h5>
-            <form action="/home" onSubmit={handleSignupSubmit}>
+            <form onSubmit={handleSignupSubmit}>
                 <p>
                     <label>Username</label><br/>
-                    <input type="text" name="first_name" required />
+                    <input type="text" name="username" required />
                 </p>
                 <p>
                     <label>Email address</label><br/>
@@ -23,7 +30,7 @@ export default function SignUpPage() {
                 </p>
                 <p>
                     <label>Password</label><br/>
-                    <input type="password" name="password" requiredc />
+                    <input type="password" name="password" required />
                 </p>
                 <p>
                     <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
